5400619463de0c0355f5a599512ce106c7b6453f
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2020-12-08 11:42:29 +0100

    commit

---
+++ project/project/js/script.js
@@ -0,0 +1,12 @@
+// op basis van https://kenwheeler.github.io/slick/
+$(document).ready(function(){
+    $('.banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+  });
+
+  
