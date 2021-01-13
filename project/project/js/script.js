19213925345bfed095054021cbf7056ad747d101
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-01-13 21:51:06 +0100

    commit

--- project/project/js/script.js
+++ project/project/js/script.js
@@ -7,33 +7,30 @@ $(document).ready(function(){
         slidesToShow: 1,
         adaptiveHeight: true,
         responsive: [
-            {
-              breakpoint: 1030,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 3,
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 3,
-              }
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 3,
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
   });
 
